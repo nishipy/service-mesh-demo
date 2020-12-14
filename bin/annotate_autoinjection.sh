90eb40487bd0ddf71033f4038cd77b7430461f26
nishipy <goodisonev4@gmail.com>
2020-12-14 18:51:58 +0900

    Update bash script

--- bin/annotate_autoinjection.sh
+++ bin/annotate_autoinjection.sh
@@ -11,27 +11,7 @@ APP_NS=$2
 #Get all deployment in APP_NS namespace
 LIST=`oc get deployment --template '{{range .items}}{{.metadata.name}} {{end}}' -n ${APP_NS}`
 
-# Responsible for injecting the istio annotation that opts in a Deployment for auto injection of the envoy sidecar
-function annotateForInjection() {
-
-  echo -en "\n\nInjecting istio sidecar annotation into Deployment: $DEPLOY\n"
-
-  # 1)  Add istio inject annotion into pod.spec.template
-  echo "apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: $DEPLOY
-spec:
-  template:
-    metadata:
-      annotations:
-        sidecar.istio.io/inject: \"true\"" \
-  | oc apply -n $APP_NS -f -
-
-}
-
-
 for DEPLOY in $LIST ; do
-    #echo $d
-    annotateForInjection
+    #echo $DEPLOY
+    oc patch deployment $DEPLOY -p '{"spec":{"template":{"metadata":{"annotations":{"sidecar.istio.io/inject":"true"}}}}}'
 done
